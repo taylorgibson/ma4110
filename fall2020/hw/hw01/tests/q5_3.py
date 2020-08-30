a9b12187996d952d88b5d4d0cb5c71e30fa0c48a
Taylor Gibson <taylorgibson@gmail.com>
2020-08-30 17:40:08 -0400

    init hw01

---
+++ fall2020/hw/hw01/tests/q5_3.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q5_3',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(precalculus_relative_change, (int, float))\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> isinstance(calculus_relative_change, (int, float))\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> isinstance(multi_relative_change, (int, float))\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
