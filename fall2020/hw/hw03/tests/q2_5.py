be35771c6cf6ad7c23afa8a2ba965363d16b8a1e
Taylor Gibson <taylorgibson@gmail.com>
2020-09-22 08:57:03 -0400

    Adding HW03

---
+++ fall2020/hw/hw03/tests/q2_5.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_5', 'points': 1, 'suites': [{'cases': [{'code': '>>> visualization in [1,2,3]\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ fall2020/hw/hw03/tests/q3_1.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q3_1',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> histogram_column_x == 1 or histogram_column_x == 2\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
