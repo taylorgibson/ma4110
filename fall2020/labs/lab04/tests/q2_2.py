69348efc4063129839201826b1adb522445db9fa
Taylor Gibson <taylorgibson@gmail.com>
2020-09-23 09:18:00 -0400

    Adding lab04

---
+++ fall2020/labs/lab04/tests/q2_2.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_2',
+    'points': 1,
+    'suites': [{'cases': [{'code': ">>> str(mark_hurd_pay_string)\n'$53.25 '", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ fall2020/labs/lab04/tests/q3_4.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3_4',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> len(cash_proportion) == 102\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> import math;\n>>> math.isclose(cash_proportion.item(0), 0.01784038, rel_tol = 0.01)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
